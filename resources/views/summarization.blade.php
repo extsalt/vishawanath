af8db12e48ae666a076b9b9641a0e884f4c0db40
Ganesh Gupta <ganeshk.work@gmail.com>
2023-12-27 21:54:02 +0530

    save

--- resources/views/summarization.blade.php
+++ resources/views/summarization.blade.php
@@ -6,121 +6,29 @@
 @section('content')
 <!-- start page title section -->
 
-<section class="bg-light-gray" style="padding-top: 145px;">
+<section class="bg-light-gray no-padding-bottom" style="padding-top: 12px; padding-bottom: 300px;">
     <div class="container">
-
-        <div class="section-heading">
-            <h2>About IntelliPPT</h2>
-        </div>
-
-        <div class="row mt-60">
-                <div class="col-lg-12 col-md-12 margin-30px-bottom ">
-                <div class="service-block4 h-100">
-                    <div class="service-icon">
-                        <i class="fas fa-info"></i>
-                    </div>
-                    <div class="service-desc">
-                        <h3>About Us</h3>
-                        <p>IntelliPPT.com is created and managed by Clourobo technologies LLP, Bangalore, India. IntelliPPT is created to create good presentations from PDFs. IntelliPPT's mission is to create quality presentations for presenters. Currently, presenters are doing a tedious job by going through the PDFs and creating PPTs from them by understanding them. IntelliPPT can help them by going through the PDFs and create PPTs automatically in seconds.</p>
-                        <p>
-                        <a href='/' style='color: #0073dc;'>IntelliPPT</a> can go though the PDF files, seggregate the headings and paragraphs and then summarize the paragraphs. It then arranges the headings and summarized text into the PPT in a presentable form. This helps the College Students, Professors, Journalists, Editors, Readers, Librarians, Writers, Publishers by creating PPTs from the document
-                        </p>
-                        <p>
-                                This tool is free for everyone, secure and has easy to use UI. Within seconds it lets you convert PDF files to PPT easily.
-                        </p>
+        <div class="row">
+            <div class="col-12" style="margin: 100px 0px;">
+                <h2>Summarize PDF/DOCX and create slides in seconds!</h2>
+            </div>
+            <div class="col-12" style="margin: 50px 0px;">
+                <div class="horizontaltab" style="display: block; width: 100%; margin: 0px;">
+                    <ul class="resp-tabs-list hor_1">
+                        <li class="resp-tab-item hor_1 resp-tab-active" aria-controls="hor_1_tab_item-0" role="tab">
+                            </i> File</li>
+                        <li class="resp-tab-item hor_1" aria-controls="hor_1_tab_item-1" role="tab">Text</li>
+                    </ul>
+                    <div class="resp-tabs-container hor_1">
+                        <div class="resp-tab-content hor_1 resp-tab-content-active" aria-labelledby="hor_1_tab_item-0"
+                            style="display:block">
+                        </div>
+                        <div class="resp-tab-content hor_1" aria-labelledby="hor_1_tab_item-1" style="display:none">
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-12 col-md-12 margin-30px-bottom ">
-                <div class="service-block4 h-100">
-                    <div class="service-icon">
-                        <i class="fas fa-info"></i>
-                    </div>
-                    <div class="service-desc">
-                        <h3>Best Text Summarizer - Save your time and money with our free summary generator tool!</h3>
-                        <p>Keeping up with the most recent studies is tiresome. Each year, millions of studies, including corporate documents, white papers, and reports, are released. There are a number of articles on your desktop that have not yet been reviewed, despite the fact that your regular research tools for summarising may assist you locate the most influential or recent ones. Utilizing our free PDF article summarizer can facilitate your work. • Copy and paste your content into the open space below. • Select the number of total sentences. • By selecting the "Summarize" button, you may begin summarising and read the full summary. </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 margin-20px-bottom ">
-                <div class="service-block4 h-100">
-                    <div class="service-icon">
-                        <i class="icon-tools"></i>
-                    </div>
-                    <div class="service-desc">
-                        <h3>What Is a Summary Generator?</h3>
-			<p>Our summary tool generator helps in creating the synopsis of the provided paper automatically within seconds. The only thing you need to do is copy and paste your text or upload your text to our summarizing tools. The PDF article summarizer tool delivers instant results, which indicates less effort and more time-saving.
-
-The summary tool holds the ability to help you with different assignments provided in your school and colleges. In our summary generator, the vital thing you need to note is that it just finds the key ideas rather than paraphrasing the complete text you provide. If you are not aware of the difference between summarizing and paraphrasing, here it is.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 margin-20px-bottom ">
-                <div class="service-block4 h-100">
-                    <div class="service-icon">
-                        <i class="icon-tools"></i>
-                    </div>
-                    <div class="service-desc">
-                        <h3>Paraphrasing – a short explanation</h3>
-                        <p>When you summarize or retell any documents in your own words, it is referred to as paraphrasing. It indicates different features, making the text relevant, highlighting important parts, and clarifying the right meaning. Both summarizing and paraphrasing tools differ from each other in various aspects. You can explore thousands of free paraphrasing and summarizing tools online. Finding the right one means easier solutions. Always include our AI summarizer when you make use of any tool to get the desired output.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 margin-20px-bottom ">
-                <div class="service-block4 h-100">
-                    <div class="service-icon">
-                        <i class="icon-tools"></i>
-                    </div>
-                    <div class="service-desc">
-                        <h3>What Can Our Summarizing Tool Do?</h3>
-			<p>You can use our multi-purpose free online PDF article summarizer tools to shorten a text or for studying. Our summary generator tools are also the right option for people who find difficulties in understanding academic papers and need to cut them to their initial parts.
-Our summary tool is capable of
-
-•       Extract meaningful information from the provided text: It offers a cohesive list or summary of the essential document ideas. You need not read the complete text to get a clear understanding.
-•       Create a summary without any Copyscape: Make sure you avoid plagiarism with the PDF article summarizer tool as it automatically creates short versions out of any papers. There is no chance for them to copy other works as it's not programmed in that way.
-•       Summarize your documents with your required number of sentences: The standard-issue found in most of the summarizer tools is that they will not cut down absolute specific numbers of texts. The unique thing about our summarizer tool is that you can choose your desired number of sentences or alter them as per your requirements. The summary length is based on the development (reasoning, paraphrases, examples, or quotes) you require.
-•       Explore the right keywords in your text: it's a challenging task for understanding an academic paper. It can be done easier by assisting or highlighting the correct type of keywords.
-•       A lot of time: I have seen many of them writing texts without knowing the effectiveness of the summary generator tools. With our summarizer, you can save more time as it automates the process on its own.
-•       Help in studying: A summary is best while preparing for the exams or a mandatory part of your assignment. Making use of our PDF article summarizer will help you learn.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 margin-20px-bottom ">
-                <div class="service-block4 h-100">
-                    <div class="service-icon">
-                        <i class="icon-tools"></i>
-                    </div>
-                    <div class="service-desc">
-                        <h3>What Can You Summarize?</h3>
-			<p>Our summarizer is capable of condensing all forms of text. Our summarizer makes it simple to condense lengthy texts. Our technology is capable of compressing up to 10,000 characters at most. Our summarizer programme supports a variety of paper formats, including. •A PDF: We have the greatest answers to your question; how may summarising be performed without plagiarising? We offer a text cutter tool that can assist with any PDF format. You must choose the overall summary length and review the supplied document's condensed version. •A book: Even if creating a summary of a book is a regular assignment at your institutions, you won't have much time to choose the proper term or read them to convey the plot. This is where our summarizer comes in handy. With our tool, downloading the books in the summarizer is simple, and you will be able to write an original paper without wasting a lot of time or plagiarising. •An essay: Many of them have difficulty writing the introduction and conclusion of any PDF or constructing a thesis statement for their paper. Our summarizer functions as a conclusion or introduction generator since it emphasises the greatest topics to be presented in the essay's introduction or conclusion. •A research paper is one of the most difficult and time-consuming assignments. You may utilise our summarizer to effortlessly select the proper terms, transform academic papers into a clear report, and more. </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 margin-20px-bottom ">
-                <div class="service-block4 h-100">
-                    <div class="service-icon">
-                        <i class="icon-tools"></i>
-                    </div>
-                    <div class="service-desc">
-                        <h3>How Does Text Summarization Work?</h3>
-                        <p>It's easy to achieve automatic summarization when you search and explore the right type of data that matches your text. A paragraph or summary generator makes use of the initial sources. There are two different techniques through which the summarization is differentiated.
-1.Abstractive text summarization, which is based on the semantic relations between the phrases and words. It's just like an individual analyzer, where it analyses the provided text and delivers it in the short version in its own terms. It clearly states context is everything.
-2.Extractive text summarization, the summary here depends on weighing the importance and similarities of text or words in a provided document automatically. Our Summarizer tool makes use of the extractive text summarization as it's the best way to summarize without any Copyscape.</p>
-                    </div>
-		</div>
             </div>
         </div>
     </div>
-            <div class="col-lg-12 col-md-12 margin-20px-bottom ">
-                <div class="service-block4 h-100">
-                    <div class="service-icon">
-                        <i class="icon-tools"></i>
-                    </div>
-                    <div class="service-desc">
-                        <h4>Privacy</h4>
-                        <p> Please checke our <a href='/privacy' style='color: #0073dc;'>Privacy Policy</a>.</p>
-                    </div>
-                </div>
-            </div>
 </section>
 @endsection
